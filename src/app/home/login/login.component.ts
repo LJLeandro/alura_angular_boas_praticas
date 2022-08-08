01797adca6448e32688383472876d4e8bc66ce22
Lucas Leandro <lucasjleandro@gmail.com>
2022-08-08 19:38:17 -0300

    Validação de Campos Concluída

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -10,7 +11,8 @@ export class LoginComponent implements OnInit {
   @Input() usuario = '';
   @Input() senha = '';
 
-  constructor(private authService: AutenticacaoService) { }
+  constructor(private authService: AutenticacaoService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +21,7 @@ export class LoginComponent implements OnInit {
     this.authService.autenticar(this.usuario, this.senha).subscribe(
       () => {
         console.log('Autenticado com Sucesso.');
+        this.router.navigate(['animais']);
       },
       (error) => {
         alert('Usuário ou senha inválido');
